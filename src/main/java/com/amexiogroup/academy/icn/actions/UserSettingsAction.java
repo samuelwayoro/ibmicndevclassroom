61d6825c514ac4cdbdfbb85858f9713e74409432
Samuel WAYORO <swayoro@amexio.fr>
2025-02-28 16:39:14 +0100

    tp3 - US3-etape1 : creation de la classe UserSettingsActionJava

---
+++ src/main/java/com/amexiogroup/academy/icn/actions/UserSettingsAction.java
@@ -0,0 +1,62 @@
+package com.amexiogroup.academy.icn.actions;
+
+import java.util.Locale;
+
+import com.ibm.ecm.extension.PluginAction;
+
+public class UserSettingsAction extends PluginAction {
+
+	@Override
+	public String getActionModelClass() { // Bizarre cette méthode n'est pas héritée de PluginAction
+		//return super.getActionModelClass();
+		return "pluginTemplateDojo.actions.UserSettingsAction";
+	}
+
+	@Override
+	public String getActionFunction() {
+		// TODO Auto-generated method stub
+		return "";
+	}
+
+	@Override
+	public String getIcon() {
+		// TODO Auto-generated method stub
+		return "";
+	}
+
+	@Override
+	public String getId() {
+		// TODO Auto-generated method stub
+		return "UserSettingsAction";
+	}
+
+	@Override
+	public String getName(Locale arg0) {
+		// TODO Auto-generated method stub
+		return "User Settings";
+	}
+
+	@Override
+	public String getPrivilege() {
+		// TODO Auto-generated method stub
+		return "";
+	}
+
+	@Override
+	public String getServerTypes() {
+		// TODO Auto-generated method stub
+		return "";
+	}
+
+	@Override
+	public boolean isMultiDoc() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isGlobal() {// bizarre méthode non implémentée
+		return true;
+	}
+
+}
