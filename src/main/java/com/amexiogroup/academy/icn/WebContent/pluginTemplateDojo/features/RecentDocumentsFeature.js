a2d77179239b165568a3828eee09c40cc3673f48
Samuel WAYORO <swayoro@amexio.fr>
2025-03-14 14:42:00 +0100

    US9: debogging et amelioration

--- src/main/java/com/amexiogroup/academy/icn/WebContent/pluginTemplateDojo/features/RecentDocumentsFeature.js
+++ src/main/java/com/amexiogroup/academy/icn/WebContent/pluginTemplateDojo/features/RecentDocumentsFeature.js
@@ -1,135 +1,139 @@
 define([
-    "dojo/_base/declare",
-    "dojo/_base/lang",
-    "dojo/json",
-    "dojo/on",//UTIL POUR ECOUTER LA SAISIE DU USER DANS LE FILTRE
-    "ecm/widget/layout/_LaunchBarPane",
-    "dijit/_TemplatedMixin",
-    "dojo/text!./templates/RecentDocumentsFeature.html",
-    "dojo/i18n!../nls/pluginMessages",
+	"dojo/_base/declare",
+	"dojo/_base/lang",
+	"dojo/json",
+	"dojo/on",//UTIL POUR ECOUTER LA SAISIE DU USER DANS LE FILTRE
+	"ecm/widget/layout/_LaunchBarPane",
+	"dijit/_TemplatedMixin",
+	"dojo/text!./templates/RecentDocumentsFeature.html",
+	"dojo/i18n!../nls/pluginMessages",
 
-    // Modules pour le ContentList
-    "ecm/widget/listView/gridModules/RowContextMenu",
-    "ecm/widget/listView/gridModules/DndFromDesktopAddDoc",
-    
-    // Modules graphiques
-    "ecm/widget/listView/modules/ViewDetail",
-    "ecm/widget/listView/modules/ViewMagazine",
-    "ecm/widget/listView/modules/DocInfo",
-    "ecm/widget/listView/modules/Bar",
-    "ecm/widget/listView/modules/Toolbar"
+	// Modules pour le ContentList
+	"ecm/widget/listView/gridModules/RowContextMenu",
+	"ecm/widget/listView/gridModules/DndFromDesktopAddDoc",
+
+	// Modules graphiques
+	"ecm/widget/listView/modules/ViewDetail",
+	"ecm/widget/listView/modules/ViewMagazine",
+	"ecm/widget/listView/modules/DocInfo",
+	"ecm/widget/listView/modules/Bar",
+	"ecm/widget/listView/modules/Toolbar"
 ],
-function(
-    declare, lang, JSON, on, _LaunchBarPane, _TemplatedMixin, template, 
-    nlsMessages, RowContextMenu, DndFromDesktopAddDoc, ViewDetail, ViewMagazine, 
-    DocInfo, Bar, Toolbar
-) {
-    return declare("pluginTemplateDojo.features.RecentDocumentsFeature", [_LaunchBarPane, _TemplatedMixin], {
-        
-        templateString: template,
-        nlsMessages: null,
+	function(
+		declare, lang, JSON, on, _LaunchBarPane, _TemplatedMixin, template,
+		nlsMessages, RowContextMenu, DndFromDesktopAddDoc, ViewDetail, ViewMagazine,
+		DocInfo, Bar, Toolbar
+	) {
+		return declare("pluginTemplateDojo.features.RecentDocumentsFeature", [_LaunchBarPane, _TemplatedMixin], {
 
-        getConfigurationDijit: function() {
-            return new RecentDocumentsFeatureConfiguration();
-        },
+			templateString: template,
+			nlsMessages: null,
 
-        constructor: function() {
-            console.debug("Constructeur exécuté.");
-            this.nlsMessages = nlsMessages;
-            this.templateString = lang.replace(this.templateString, this.nlsMessages);
-        },
+			getConfigurationDijit: function() {
+				return new RecentDocumentsFeatureConfiguration();
+			},
 
-        postCreate: function() {
-            console.debug("Début postCreate.");
-            this.inherited(arguments);
+			constructor: function() {
+				console.debug("Constructeur exécuté.");
+				this.nlsMessages = nlsMessages;
+				this.templateString = lang.replace(this.templateString, this.nlsMessages);
+			},
 
-            this.recentDocuments.setContentListModules(this._getContentListModules());
-            this.recentDocuments.setGridExtensionModules(this._getGridExtensionModules());
+			postCreate: function() {
+				console.debug("Début postCreate.");
+				this.inherited(arguments);
 
-            // Attacher l'événement de filtre sur le champ input
-            this._setupFilterListener();
+				this.recentDocuments.setContentListModules(this._getContentListModules());
+				this.recentDocuments.setGridExtensionModules(this._getGridExtensionModules());
 
-            console.debug("Fin postCreate.");
-        },
+				// Attacher l'événement de filtre sur le champ input
+				this._setupFilterListener();
 
-        loadContent: function(filterValue) {
-            let days = 7;
-            let configurationString = {};
+				console.debug("Fin postCreate.");
+			},
 
-            if (this.feature) {
-                configurationString = JSON.parse(this.feature.pluginConfiguration);
-                days = configurationString.days;
-            }
+			loadContent: function(filterValue) {
+				let days = 7;
+				let configurationString = {};
 
-            console.log("Nombre de jours utilisé pour la requête :", days);
+				if (this.feature) {
+					configurationString = JSON.parse(this.feature.pluginConfiguration);
+					days = configurationString.days;
+				}
 
-            let query = `SELECT * FROM Document WHERE DateCreated > Now() - TimeSpan(${days},'Days')`;
+				//console.log("Nombre de jours utilisé pour la requête :", days);
 
-            // Appliquer le filtre si du texte est saisi
-            if (filterValue && filterValue.length > 0) {
-                query += ` AND DocumentTitle LIKE '%${filterValue}%'`;
-            }
+				let query = `SELECT * FROM Document WHERE DateCreated > Now() - TimeSpan(${days},'Days')`;
 
-            query += " ORDER BY DateCreated Desc";
+				// Appliquer le filtre si du texte est saisi
+				if (filterValue && filterValue.length > 0) {
+					query += ` AND DocumentTitle LIKE '%${filterValue}%'`;
+				}
 
-            const searchQuery = new ecm.model.SearchQuery({
-                repository: ecm.model.desktop.repositories[0],
-                query: query,
-                pageSize: 200,
-                resultsDisplay: {
-                    columns: ["DocumentTitle", "DateCreated", "Creator", "ContentSize", "LastModifier"],
-                    sortBy: "DateCreated",
-                    sortAsc: false
-                }
-            });
+				query += " ORDER BY DateCreated Desc";
 
-            searchQuery.search(
-                dojo.hitch(this, function(resultSet) {
-                    console.info("Documents trouvés :", resultSet);
-                    this.recentDocuments.setResultSet(resultSet);
-                }),
-                null, null, null,
-                dojo.hitch(this, function(error) {
-                    console.error("Erreur de recherche :", error);
-                })
-            );
-        },
+				const searchQuery = new ecm.model.SearchQuery({
+					repository: ecm.model.desktop.repositories[0],
+					query: query,
+					pageSize: 200,
+					resultsDisplay: {
+						columns: ["DocumentTitle", "DateCreated", "Creator", "ContentSize", "LastModifier"],
+						sortBy: "DateCreated",
+						sortAsc: false
+					}
+				});
 
-        _setupFilterListener: function() {
-            if (this.filterInput) {
-                on(this.filterInput, "keyup", dojo.hitch(this, function() {
-                    let filterText = this.filterInput.value.trim();
-                    console.log("Filtrage avec :", filterText);
-                    this.loadContent(filterText);
-                }));
-            }
-        },
+				searchQuery.search(
+					dojo.hitch(this, function(resultSet) {
+						//console.info("Documents trouvés :", resultSet);
+						this.recentDocuments.setResultSet(resultSet);
+					}),
+					null, null, null,
+					dojo.hitch(this, function(error) {
+						console.error("Erreur de recherche :", error);
+					})
+				);
+			},
 
-        _getContentListModules: function() {
-            const modules = [];
+			_setupFilterListener: function() {
+				console.debug("debut de la méthode _setupFilterListener");
+				if (this.filterInput) {
+					console.debug("presence d'une saisie...");
+					on(this.filterInput, "keyup", dojo.hitch(this, function() {
+						console.debug("DONNEE SAISIE ", this.filterInput.value.trim());
+						let filterText = this.filterInput.value.trim();
+						console.log("Filtrage avec :", filterText);
+						this.loadContent(filterText);
+					}));
+				}
+				console.debug("debut de la méthode _setupFilterListener");
+			},
 
-            modules.push({
-                moduleClass: DocInfo,
-                selectAutoOpen: true,
-                showSystemProps: false
-            });
+			_getContentListModules: function() {
+				const modules = [];
 
-            const viewModules = [ViewDetail, ViewMagazine];
+				modules.push({
+					moduleClass: DocInfo,
+					selectAutoOpen: true,
+					showSystemProps: false
+				});
 
-            modules.push({
-                moduleClass: Bar,
-                top: [[[
-                    { moduleClasses: viewModules, "className": "BarViewModules" },
-                    { moduleClass: Toolbar }
-                ]]],
-                bottom: [[]]
-            });
+				const viewModules = [ViewDetail, ViewMagazine];
 
-            return modules;
-        },
+				modules.push({
+					moduleClass: Bar,
+					top: [[[
+						{ moduleClasses: viewModules, "className": "BarViewModules" },
+						{ moduleClass: Toolbar }
+					]]],
+					bottom: [[]]
+				});
 
-        _getGridExtensionModules: function() {
-            return [RowContextMenu, DndFromDesktopAddDoc];
-        }
-    });
-});
+				return modules;
+			},
+
+			_getGridExtensionModules: function() {
+				return [RowContextMenu, DndFromDesktopAddDoc];
+			}
+		});
+	});
